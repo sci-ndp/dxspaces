acc59d8d1dd06dac5a94f2edda1184f022c2285d
Philip Davis <philipdavis01@gmail.com>
2024-08-26 10:31:27 -0400

    Add long_description parameter

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup
 
+
+
 setup(name='DXSpaces',
       version='0.0.1',
       description='Client library for RESTful DataSpaces',
+      long_description=open('README.md').read(),
+      long_description_content_type='text/markdown',
       author='Philip Davis',
       author_email='philip.davis@sci.utah.edu',
       url='https://github.com/sci-ndp/dxspaces',
